f5d290b90cf3aee4f399252c29a5ae2941675538
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-03-29 03:26:32 +0200

    add ExecutorService and RestTemplate beans

--- src/main/java/nl/tudelft/distributed/team17/Config.java
+++ src/main/java/nl/tudelft/distributed/team17/Config.java
@@ -1,11 +1,25 @@
 package nl.tudelft.distributed.team17;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class Config
 {
+	@Bean
+	ExecutorService executorService()
+	{
+		return Executors.newCachedThreadPool();
+	}
 
+	@Bean
+	RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
 }
