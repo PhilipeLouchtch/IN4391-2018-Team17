de4be181079f82f4bcd18a01e8a35ee96c3dc529
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-03-31 11:57:51 +0200

    let WorldStateGenerator create 5 dragons

--- src/main/java/nl/tudelft/distributed/team17/service/WorldStateGenerator.java
+++ src/main/java/nl/tudelft/distributed/team17/service/WorldStateGenerator.java
@@ -1,5 +1,6 @@
 package nl.tudelft.distributed.team17.service;
 
+import nl.tudelft.distributed.team17.model.UnitType;
 import nl.tudelft.distributed.team17.model.WorldState;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,12 @@ public class WorldStateGenerator
 	public WorldState generateNewWorldState()
 	{
 		WorldState initialWorldState = WorldState.initial();
+
+		initialWorldState.spawnUnit("henk", UnitType.DRAGON);
+		initialWorldState.spawnUnit("jan", UnitType.DRAGON);
+		initialWorldState.spawnUnit("klaas", UnitType.DRAGON);
+		initialWorldState.spawnUnit("albert", UnitType.DRAGON);
+		initialWorldState.spawnUnit("bob", UnitType.DRAGON);
 
 		// TODO: actually generate the world
 		return initialWorldState;
